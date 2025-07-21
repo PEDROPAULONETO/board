e3eeb743923ea4414f33845324f9a02e912964da
PEDRO PAULO DA SILVA NETO <pedro_pauloneto@outlook.com>
2025-07-21 20:22:19 -0300

    Update Main.java
    adicionar  mensagens de erro mais informativas ao usuário

--- src/main/java/br/com/dio/Main.java
+++ src/main/java/br/com/dio/Main.java
@@ -9,12 +9,14 @@ import static br.com.dio.persistence.config.ConnectionConfig.getConnection;
 
 
 public class Main {
-
-    public static void main(String[] args) throws SQLException {
-        try(var connection = getConnection()){
+    public static void main(String[] args) {
+        try (var connection = getConnection()) {
             new MigrationStrategy(connection).executeMigration();
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar ou migrar o banco de dados: " + e.getMessage());
+            return;
         }
+
         new MainMenu().execute();
     }
-
 }
